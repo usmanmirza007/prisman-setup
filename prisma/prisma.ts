cd530781afd9bd3dba7c16b651ab9348a5b5dfa8
Muhammad Usman <usmandgk107@gmail.com>
2026-05-05 14:53:40 +0500

    Modularize Prisma schema, add age column to User, and improve database connection logging

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -5,4 +5,24 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
 
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!)
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({
+  adapter,
+  log: ['query', 'info', 'warn', 'error'],
+})
+
+// Better connection test
+async function testConnection() {
+  try {
+    await prisma.$queryRaw`SELECT 1 as test`
+    console.log('yoyoyoyo');
+
+    console.log('✅ Prisma successfully connected to the database.')
+  } catch (err: any) {
+    console.error('❌ Connection failed:', err.message)
+    if (err.meta?.driverAdapterError) {
+      console.error('Driver error details:', err.meta.driverAdapterError)
+    }
+  }
+}
+
+testConnection()
